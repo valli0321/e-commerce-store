0050891ab336e177ab6d154e5087b9b0eaf1e11c
Shriballabh Bhatt <shriballabhbhatt1234@gmail.com>
2025-08-31 20:54:34 +0530

    Add currency, formatter function for curreny, no-results and icon button component

---
+++ components/ui/currency.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+import { formatter } from '@/lib/utils';
+
+interface CurrencyProps {
+    value: string | number;
+}
+
+const Currency: React.FC<CurrencyProps> = ({
+    value
+}) => {
+
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted){
+        return null;
+    }
+
+    return (
+        <div className='font-semibold'>
+            {formatter.format(Number(value))}
+        </div>
+  )
+}
+
+export default Currency
---
+++ components/ui/icon-button.tsx
@@ -0,0 +1,28 @@
+import React, { MouseEventHandler } from 'react'
+import { cn } from '@/lib/utils';
+
+interface IconButtonProps {
+    onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+    className?: string;
+    icon: React.ReactElement
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+    onClick,
+    className,
+    icon
+}) => {
+  return (
+    <button
+        onClick={onClick}
+        className={cn(
+            "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
+            className
+        )}
+    >
+        {icon}
+    </button>
+  )
+}
+
+export default IconButton
