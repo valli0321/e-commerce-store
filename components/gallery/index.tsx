f6ea7722cbf45563de8600d950f200f3238bd63e
Shriballabh Bhatt <shriballabhbhatt1234@gmail.com>
2025-09-01 10:46:06 +0530

    Add product list, info component and gallery for product images

---
+++ components/gallery/index.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from 'react';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import Image from 'next/image';
+
+import { Image as ImageType } from '@/types';
+
+import GalleryTab from './gallery-tab';
+
+interface GalleryProps {
+    images: ImageType[];
+}
+
+const Gallery: React.FC<GalleryProps> = ({
+    images
+}) => {
+    return (
+        <TabGroup as="div" className="flex flex-col-reverse">
+            <div className='mx-auto hidden mt-6 w-full max-w-2xl sm:block lg:max-w-none'>
+                <TabList className="grid grid-cols-4 gap-6">
+                    {
+                        images?.map((image) => (
+                            <GalleryTab  key={image?.id} image={image} />
+                        ))
+                    }
+                </TabList>
+            </div>
+            <TabPanels className="aspect-square w-full">
+                {
+                    images?.map((image) => (
+                        <TabPanel key={image.id}>
+                            <div className='aspect-square relative h-full w-full sm:rounded-lg overflow-hidden'>
+                                <Image
+                                    fill
+                                    src={image?.url}
+                                    alt="Image"
+                                    className="object-cover object-center"
+                                />
+                            </div>
+                        </TabPanel>
+                    ))
+                }
+            </TabPanels>
+        </TabGroup>
+    )
+}
+
+export default Gallery
