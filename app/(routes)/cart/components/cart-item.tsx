af4b030122a5ac92cc2f2843c08e47ae95b962ec
Shriballabh Bhatt <shriballabhbhatt1234@gmail.com>
2025-09-02 15:06:17 +0530

    Add cart page

---
+++ app/(routes)/cart/components/cart-item.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React from 'react';
+import Image from 'next/image';
+import { X } from 'lucide-react';
+
+import { useCart } from '@/hooks/use-cart';
+import { Product } from '@/types';
+
+import IconButton from '@/components/ui/icon-button';
+import Currency from '@/components/ui/currency';
+
+interface CartItemProps {
+    data: Product;
+};
+
+const CartItem: React.FC<CartItemProps> = ({
+    data
+}) => {
+    const cart = useCart();
+    const [isMounted, setIsMounted] = React.useState(false);
+
+    React.useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted){
+        return null;
+    }
+
+    return (
+        <li className='flex py-6 border-b border-gray-300'>
+            <div className='relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48'>
+                <Image
+                    fill
+                    src={data?.images[0]?.url}
+                    alt='Image'
+                    className='object-cover object-center'
+                />
+            </div>
+            <div className='relative ml-4 flex flex-1 flex-col justify-between sm:ml-6'>
+                <div className='absolute z-10 top-0 right-0'>
+                    <IconButton icon={<X size={15} />} onClick={() => cart.removeItemFromCart(data)} />
+                </div>
+                <div className='relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0'>
+                    <div className='flex justify-between'>
+                        <p className='text-lg font-semibold text-black'>
+                            {data?.name}
+                        </p>
+                    </div>
+                    <div className='mt-1 text-sm flex'>
+                        <p>{data?.color?.name}</p>
+                        <p>{data?.size?.name}</p>
+                    </div>
+                    <Currency value={data?.price}/>
+                </div>
+            </div>
+        </li>
+    )
+}
+
+export default CartItem
---
+++ app/(routes)/cart/page.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import React from 'react';
+
+import { useCart } from '@/hooks/use-cart';
+
+import Container from '@/components/ui/container';
+import CartItem from './components/cart-item';
+import Summary from './components/summary';
+
+const CartPage = () => {
+
+    const [isMounted, setIsMounted] = React.useState(false);
+    const cart = useCart();
+
+    React.useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted){
+        return null;
+    }
+
+
+    return (
+        <div className='bg-white'>
+            <Container>
+                <div className='px-4 py-16 sm:px-6 lg:px-8'>
+                    <h3 className='text-3xl font-bold text-black'>Shopping Cart</h3>
+                    <div className='mt-12 lg:grid-cols-12 lg:items-start gap-x-2'>
+                        <div className='lg:col-span-7'>
+                            {cart.isEmpty && <p className='text-neutral-500'>No items added to Cart.</p>}
+                            <ul>
+                                {cart.items.map((item) => (
+                                    <CartItem
+                                        key={item?.id}
+                                        data={item}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
+                        <Summary/>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default CartPage
